a2f62d888e2548fbb1235c70c0d339deee7bfb66
Rahugg <rahugg07@gmail.com>
2023-10-28 10:17:51 +0600

    grpc-client in server

--- BackEnd/project/app/server.go
+++ BackEnd/project/app/server.go
@@ -2,14 +2,19 @@ package main
 
 import (
 	"context"
+	"log"
+	"time"
 
 	"github.com/gin-gonic/gin"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
 
 	"project/app/controller"
 	"project/source/app/services"
 	"project/source/domain/entity"
 	"project/source/infrastructure/postgresql"
 	"project/source/infrastructure/repositories"
+	pb "project/your/go/package" // Replace with the path to your proto package
 )
 
 var (
@@ -22,7 +27,11 @@ var (
 	Controller            = controller.New(*service)
 )
 
-func main1() {
+const (
+	address = "localhost:50051"
+)
+
+func main() {
 	server := gin.Default()
 	//server.Use(cors.New(cors.Config{
 	//	AllowOrigins: []string{"http://localhost:4200"},
@@ -41,7 +50,7 @@ func main1() {
 		events.GET("/:id", Controller.FindUserEvents)
 	}
 	server.Run()
-
+	gRPC()
 }
 func CORSMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
@@ -58,4 +67,30 @@ func CORSMiddleware() gin.HandlerFunc {
 
 		c.Next()
 	}
+}
+func gRPC() {
+	conn, err := grpc.Dial(address, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		log.Fatalf("Did not connect: %v", err)
+	}
+	defer conn.Close()
+
+	client := pb.NewYourServiceClient(conn)
+
+	login := "YourTestLogin"
+	password := "YourTestPassword"
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*10)
+	defer cancel()
+	log.Printf("Server listening at %v", address)
+
+	resp, err := client.Login(ctx, &pb.YourLoginRequest{
+		Login:    login,
+		Password: password,
+	})
+	if err != nil {
+		log.Fatalf("Could not login: %v", err)
+	}
+
+	log.Printf("Received token: %s", resp)
 }
